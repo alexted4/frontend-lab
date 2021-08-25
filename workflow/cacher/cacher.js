73678332c52e1fbe8ff0c9babb50752dae9cc6e0
alexted4 <79405751+alexted4@users.noreply.github.com>
2021-08-25 19:41:06 +0700

    fix: fixed lets to consts

--- workflow/cacher/cacher.js
+++ workflow/cacher/cacher.js
@@ -2,12 +2,12 @@ var Cacher = function(){
     this.cache = new Map();
     this.withCache = function(func){
         return (...args) => {
-            let key = args.join();
+            const key = args.join();
             if (this.cache.has(key)){
                 console.log('found');
                 return this.cache.get(key);
             } else {
-                let res = func(...args);
+                const res = func(...args);
                 this.cache.set(key, res);
                 console.log('created');
                 return res;
@@ -24,7 +24,7 @@ const calculate = document.getElementById("calculate");
 const output = document.getElementById("output");
 
 calculate.addEventListener('click', () => {
-    let arr = numbers.value.split(',');
+    const arr = numbers.value.split(',');
     const result = cachedFunc(arr);
     output.innerHTML = "Result = " + result;
 })
